5e89440776b69a58812ef195de33aa2004f06a66
eungbin <programmer97@naver.com>
2024-01-03 18:18:20 +0900

    팀 정하기 1팀, 2팀 랜덤 추가

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,9 +1,34 @@
 import React, {  } from 'react';
+import './css/Team.css';
 
 export default function Team() {
+  const firstTeamId = ['first1', 'first2', 'first3', 'first4'];
+  const secondTeamId = ['second1', 'second2', 'second3', 'second4'];
+
   return (
-    <section className='team-main-contaier'>
-      
+    <section className='team-main-container'>
+      <div className='team-box first'>
+        <span className='box-header'>1팀 고정</span>
+        <section className='input-container'>
+          {firstTeamId.map((v, idx) => (
+            <input type='text' className='team-input' id={v} key={idx} />
+          ))}
+        </section>
+      </div>
+      <div className='team-box second'>
+        <span className='box-header'>2팀 고정</span>
+        <section className='input-container'>
+          {secondTeamId.map((v, idx) => (
+            <input type='text' className='team-input' id={v} key={idx} />
+          ))}
+        </section>
+      </div>
+      <div className='team-box random'>
+        <span className='box-header'>랜덤</span>
+        <section className='input-container'>
+          <div className='team-input'>생성</div>
+        </section>
+      </div>
     </section>
   );
 }
