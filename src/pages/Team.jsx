9a0bc1a11e126c4596c8addef88f5857b3208238
eungbin <programmer97@naver.com>
2024-01-09 00:14:28 +0900

    랜덤 라인 기능 추가중

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,30 +1,13 @@
 import React, { useState } from 'react';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
 import './css/Team.css';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function Team() {
   const firstTeamId = ['first1', 'first2', 'first3', 'first4'];
   const secondTeamId = ['second1', 'second2', 'second3', 'second4'];
   const [randomCount, setRandomCount] = useState(0);
-  const [open, setOpen] = useState(false); // modal open/close state
-  const [team_1, setTeam_1] = useState([]);
-  const [team_2, setTeam_2] = useState([]);
-
-  const handleClose = () => { setOpen(false); }
-  const handleOpen = () => { setOpen(true); }
+  const [team_1, setTeam_1] = useState('');
+  const [team_2, setTeam_2] = useState('');
+  const [status, setStatus] = useState(''); // init: 초기, team: 팀이 짜여진 상태, line: 라인까지 선정된 상태
 
   /**
    * 랜덤 팀원 입력창 생성 함수
@@ -78,23 +61,25 @@ export default function Team() {
     if(total > 0) { console.log('인원이 10명보다 적습니다!'); return; }
     if(total < 0) { console.log('인원이 10명보다 많습니다!'); return; }
 
-    setTeam_1([...firstTeam]);
-    setTeam_2([...secondTeam]);
-    handleOpen();
+    setTeam_1(firstTeam.join(', '));
+    setTeam_2(secondTeam.join(', '));
+  }
+
+  const randomLine = () => {
+    if(team_1 !== '' && team_2 !== '') {
+      createLine(team_1);
+    } else { 
+      console.log('팀이 정해지지 않았습니다!'); return;  
+    }
+  }
+  const createLine = (team) => {
+    const listTeam = team.split(', ');
+    listTeam.sort(() => Math.random() - 0.5);
+    return listTeam;
   }
 
   return (
     <section className='team-main-container'>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          {team_1}
-        </Box>
-      </Modal>
       <section className='team-inner-container'>
         <div className='team-box first'>
           <span className='box-header'>1팀 고정</span>
@@ -121,6 +106,13 @@ export default function Team() {
       </section>
       <section className='container-btn'>
         <div className='btn-action' onClick={createTeam}>팀 짜기</div>
+        <div className='btn-action' onClick={randomLine}>라인 정하기</div>
+      </section>
+      <section className='container-result'>
+        <div className='box-result'>
+          <p className='draggable'>{team_1}</p>
+          <p className='draggable'>{team_2}</p>
+        </div>
       </section>
     </section>
   );
